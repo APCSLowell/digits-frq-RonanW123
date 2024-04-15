a3e8fe2756e9c6ea0a174a2d224f0aee82336896
RonanW123 <142534416+RonanW123@users.noreply.github.com>
2024-04-15 11:25:51 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,13 +7,17 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+	   String sNum = num + "";
+	   for(int i = 0; i < sNum.length(); i++)
+		digitsList.add(Integer.parseInt(sNum.charAt(i) + ""));
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
-
+	    for(int i = 0; i < digitList.size()-1; i++)
+		if(digitList.get(i) > digitList.get(i+1))
+		     return false;
+	     return true;
 	}
 	
 	public String toString()
